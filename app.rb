5be153d6dbdb676f53224a0ee5e1b3991a6a2c85
rbeau12 <107488237+rbeau12@users.noreply.github.com>
2024-10-22 06:10:58 +0000

    Working

--- app.rb
+++ app.rb
@@ -2,8 +2,43 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homeTemp)
+end
+
+get("/rock") do
+  moves=['rock','paper','scissors']
+  @oppMove=moves.sample
+  if @oppMove=='rock'
+    @outcome='tied!'
+  elsif @oppMove=='paper'
+    @outcome='lost!'
+  else
+      @outcome='won!'
+  end
+  erb(:rockTemp)
+end
+
+get("/scissors") do
+  moves=['rock','paper','scissors']
+  @oppMove=moves.sample
+  if @oppMove=='rock'
+    @outcome="lost!"
+  elsif @oppMove=='paper'
+    @outcome="won!"
+  else
+    @outcome="tied!"
+  end
+  erb(:scissorsTemp)
+end
+get("/paper") do
+  moves=['rock','paper','scissors']
+  @oppMove=moves.sample
+  if @oppMove=='rock'
+    @outcome='won!'
+  elsif @oppMove=='paper'
+    @outcome='tied!'
+  else @oppMove=='scissors'
+    @outcome='lost!'
+  end
+  erb(:paperTemp)
 end
